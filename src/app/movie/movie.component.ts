e518c347fa4380e18b549b1a7f0f4c63ab1f321f
Carrie West <carrie105violet@gmail.com>
2022-03-14 22:52:47 -0400

    Excluded adult titles from poster search. Improved search accuracy.

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -15,26 +15,55 @@ export class MovieComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    console.log(this.movie?.name)
-    try{
-      var dataResponse = this.getMetadata('(')
+    this.movie!.name = this.movie!.name.replace('\"','\'')
+
+    var dataResponse = this.getMetadata('(')
+
+    
+    dataResponse.subscribe((data:any)=>{
+      if(data.res)
+     this.movie!.poster_url = 'https://image.tmdb.org/t/p/w220_and_h330_face'+ data.results[0].poster_path
+    }
+     )
+
+    if (this.movie!.poster_url){
+      dataResponse.subscribe((data:any)=> this.movie!.overview = data.results[0].overview)
+    }else{
+      var dataResponse = this.getMetadata(':')
       dataResponse.subscribe((data:any)=> this.movie!.poster_url = 'https://image.tmdb.org/t/p/w220_and_h330_face'+ data.results[0].poster_path)
       dataResponse.subscribe((data:any)=> this.movie!.overview = data.results[0].overview)
-    }catch (TypeError){
-      console.log("No poster found")
     }
+
   }
 
-  getMetadata(delimiter: string){
-    var title = this.movie?.name.split(delimiter)[0]
-    console.log(title)
+  getMetadata(colon?: string){
+    const splitter = this.movie?.name.split('(');
+    console.log(splitter)
+
+    var title = splitter![0].trim().replace('\"', '\'');
+    if (colon){
+      title = title.split(':')[0]
+      console.log(title)
+    }
+
+    var searchUrl = 'https://api.themoviedb.org/3/search/multi?api_key=f213c4954a309bf85342338cab0ba8a6&language=en-US&query='+ title + '&page=1';
+
+    if (splitter!.length > 1){
+      var year = splitter![1].replace(')','').replace('-','').trim();
+
+      var searchUrl = 'https://api.themoviedb.org/3/search/multi?api_key=f213c4954a309bf85342338cab0ba8a6&language=en-US&query='+ title +'&year=' + year +'&page=1';
+    }
+
     let header_node = {
       headers: new HttpHeaders(
           { 'rejectUnauthorized': 'false',
             }),
-      
+    
     };
-    return this.http.get<JSON>('https://api.themoviedb.org/3/search/multi?api_key=f213c4954a309bf85342338cab0ba8a6&language=en-US&query='+ title + '&page=1&include_adult=true', {responseType:'json'})
+    console.log(searchUrl)
+
+
+    return this.http.get<JSON>(searchUrl, {responseType:'json'})
   }
 
 }
